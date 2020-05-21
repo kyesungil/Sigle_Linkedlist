23d6543f73faf0b10c1aadb3a9dec39358ce73d0
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-05-21 18:14:27 +0900

    05. 저장된 데이터를 화면에 출력

---
+++ 05.display_list.c
@@ -0,0 +1,22 @@
+﻿/*
+	5. display_list(List* list)
+		- 저장된 데이터를 화면에 출력
+*/
+
+void display_list(List* list)
+{
+	ListElmt* search;
+
+	if (list->head == NULL)
+	{
+		printf("No items\n");
+		return;
+	}
+	// head부터시작해서 next포인터로 하나씩 노드를 지나간다. 마지막으로 next포인터가 NULL이면 종료
+	for (search = list->head; search != NULL; search = search->next)
+	{
+		printf("[ID:%d Name:%s]\n",
+			((dv_ptr)search->data)->dv_id, ((dv_ptr)search->data)->dv_name);
+	}
+	return;
+}

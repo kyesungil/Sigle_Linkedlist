6e01b206a089e81f3849a640f395e019d32ac6e7
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-05-21 18:08:10 +0900

    07. 오름차순으로 정렬하기

---
+++ 07.list_sorted.c
@@ -0,0 +1,20 @@
+﻿/* 오름차순으로 정렬하기 */
+void list_sorted(List* list, device* tmp_data)
+{
+	ListElmt* search;
+	ListElmt* search2;
+
+	for (search = list->head; search != NULL; search = search->next)
+	{
+		for (search2 = search->next; search2 != NULL; search2 = search2->next)
+		{
+			if (strcmp(((dv_ptr)search->data)->dv_name, ((dv_ptr)search2->data)->dv_name) > 0)
+			{
+				tmp_data = search->data;
+				search->data = search2->data;
+				search2->data = tmp_data;
+			}
+		}
+	}
+	return;
+}

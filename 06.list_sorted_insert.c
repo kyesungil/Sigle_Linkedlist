331d5811a965a9569d4df9e87ba6d075df94fd41
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-05-21 18:15:00 +0900

    06. 오름차순으로 데이터 넣기

---
+++ 06.list_sorted_insert.c
@@ -0,0 +1,16 @@
+﻿/* 응용 - 오름차순으로 데이터 넣기 */
+int list_sorted_insert(List* list, device* dev_data)
+{
+	ListElmt* search;
+	ListElmt* prev = NULL; // head 앞에 데이터가 들어갈수 있기 때문에 NULL로 초기화
+
+	// 데이터 오름차순으로
+	for (search = list->head; search != NULL; search = search->next)
+	{
+		if (strcmp(((dv_ptr)search->data)->dv_name, dev_data->dv_name) > 0)
+			break;
+		else
+			prev = search; // 이전 노드 저장
+	}
+	return list_ins_next(list, prev, dev_data);
+}

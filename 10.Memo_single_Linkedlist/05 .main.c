15fe1e0e5edf20cb20582eb0c413b8ceee5ce35e
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-05-29 18:33:05 +0900

    05. main

---
+++ 10.Memo_single_Linkedlist/05 .main.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Memo.h"
+
+
+
+int main()
+{
+	List list;
+	list_init(&list, NULL);
+
+
+	Total_menu(&list);
+
+	return 0;
+}
